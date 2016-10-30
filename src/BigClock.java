c3c53c920831b2f3016a23187edab0d7a7b13a94
Nathan Ahrens <nathanmahrens@gmail.com>
2016-10-30 16:25:57 -0400

    Changed which library imported for key mnemonics

--- src/BigClock.java
+++ src/BigClock.java
@@ -7,6 +7,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
+import java.awt.event.KeyEvent;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -28,8 +29,6 @@ import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
-
-import com.sun.glass.events.KeyEvent;
 
 public class BigClock
 {
